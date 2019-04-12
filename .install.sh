a36f5a5ba99fd687c1ff8a5de81d54545e01d9c0
Kris Ezra <kris.ezra@gmail.com>
2019-04-12 15:52:50 -0400

    "Make the install friendlier"

--- .install.sh
+++ .install.sh
@@ -3,10 +3,17 @@ shopt -s dotglob
 
 # Get the directory of this file
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+if [ -d ~/.vim ]; then
+    mv ~/.vim ~/.vim.old
+fi
+
 mkdir -p ~/.vim
 mv $DIR/* ~/.vim/
 
 cd ~/.vim
+
+echo "source ~/.vim/.bash_vim" >> ~/.bashrc
 
 # Set up the submodules (Vundle)
 git submodule init
