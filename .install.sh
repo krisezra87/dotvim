c9e8a7c6057d3479a0498ae5f85477ca9086cc5b
Kris Ezra <kris.ezra@gmail.com>
2019-04-12 01:01:57 -0400

    Move to the proper directory before extraction

--- .install.sh
+++ .install.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+shopt -s dotglob
+
+# Get the directory of this file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+mkdir -p ~/.vim
+mv $DIR/* ~/.vim/
+
+cd ~/.vim
 
 # Set up the submodules (Vundle)
 git submodule init
